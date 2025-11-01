328df0d73115f5ded3a924c73768bf2e79df9b7e
Mansi5164 <156164354+Mansi5164@users.noreply.github.com>
2025-11-01 22:17:14 +0530

    entities created

---
+++ src/main/java/com/coding/shuttle/airBnb/App/entity/Hotel.java
@@ -0,0 +1,42 @@
+package com.coding.shuttle.airBnb.App.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String city;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] photos;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] amenities;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Embedded
+    private HotelContactInfo hotelContactInfo;
+
+}
---
+++ src/main/java/com/coding/shuttle/airBnb/App/entity/HotelContactInfo.java
@@ -0,0 +1,14 @@
+package com.coding.shuttle.airBnb.App.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Embeddable
+public class HotelContactInfo {
+    private String address;
+    private String phoneNumber;
+
+}
